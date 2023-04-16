f8d295cfd4b6cb035bf9d4bc7eb79aaedad80c6d
Adham <64488511+adhamkamala@users.noreply.github.com>
2023-04-16 21:54:15 +0200

    adding basics of  padssystem

---
+++ Assets/PadsSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PadsSystem : MonoBehaviour
+{
+    public GameObject leftHandPad;
+    public GameObject rightHandPad;
+
+    public Material leftHandMaterial;
+    public Material rightHandMaterial;
+
+    public TextMeshPro leftHandText;
+    public TextMeshPro rightHandText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer LeftHandPad = leftHandPad.GetComponent<Renderer>();
+        Renderer RightHandPad = rightHandPad.GetComponent<Renderer>();
+
+        // Set the new material
+        LeftHandPad.material = leftHandMaterial;
+        RightHandPad.material = leftHandMaterial;
+
+        leftHandText.SetText("AD");
+        rightHandText.SetText("X");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
