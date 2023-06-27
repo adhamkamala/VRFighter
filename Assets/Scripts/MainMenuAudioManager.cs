9f33a985535c418175dbdc07bbd8bfcb67d5eca7
KevinAndradeSae-Lew <128732033+KevinAndradeSae-Lew@users.noreply.github.com>
2023-06-27 21:36:53 +0200

    audios angepasst

---
+++ Assets/Scripts/MainMenuAudioManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MainMenüAudioManager : MonoBehaviour
+{
+    [Header("----------- AudioSource ------------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("----------- AudioSource ------------")]
+    public AudioClip background1;
+    public AudioClip background2;
+    public AudioClip rightPunch;
+    public AudioClip LeftPunch;
+
+    private void Start()
+    {
+        musicSource.clip = background1;
+        musicSource.Play();
+    }
+
+  
+
+}
